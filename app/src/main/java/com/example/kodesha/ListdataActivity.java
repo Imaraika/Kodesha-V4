3a7e1a39284c7d880ccbc05c85f8b549a25fb062
Imaraika <inange2013@gmail.com>
2019-10-14 08:33:44 +0200

    changes

--- app/src/main/java/com/example/kodesha/ListdataActivity.java
+++ app/src/main/java/com/example/kodesha/ListdataActivity.java
@@ -2,13 +2,46 @@ package com.example.kodesha;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class ListdataActivity extends AppCompatActivity {
+    TextView listdata;
+    ImageView detailimageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_listdata);
+
+        listdata= findViewById(R.id.listdata);
+        detailimageView = findViewById(R.id.imageViewdetail);
+
+        Intent intent = getIntent();
+        int receivedImage = intent.getIntExtra("image",0);
+        String receivedName =  intent.getStringExtra("name");
+
+
+        listdata.setText(receivedName);
+        detailimageView.setImageResource(receivedImage);
+        //enable back Button
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+    //getting back to listview
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            onBackPressed();
+        }
+        return super.onOptionsItemSelected(item);
+
+    }
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
     }
 }
+
