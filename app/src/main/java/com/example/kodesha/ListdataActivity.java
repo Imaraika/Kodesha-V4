7a25c5f7317c32e6c43a5fdc5996cb45717a9b7f
Imaraika <inange2013@gmail.com>
2019-10-14 08:12:58 +0200

    changes

--- app/src/main/java/com/example/kodesha/ListdataActivity.java
+++ app/src/main/java/com/example/kodesha/ListdataActivity.java
@@ -1,4 +1,14 @@
 package com.example.kodesha;
 
-public class ListdataActivity {
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ListdataActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_listdata);
+    }
 }
